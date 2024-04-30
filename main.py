8ecac13fa46a0e23f9f1ad964fa24a68131182b7
ArmedWings <armedwings@yandex.ru>
2024-04-30 07:39:41 +0300

    Style update
    Added label

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFrame, QSizePolicy, QSpacerItem, QGraphicsOpacityEffect
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFrame, \
+    QSizePolicy, QSpacerItem, QGraphicsOpacityEffect
 from PySide6.QtCore import Qt, QSize, QPropertyAnimation, QEasingCurve, QRect, QMargins, QTimer
 from PySide6.QtGui import QPixmap, QPainter, QBrush, QLinearGradient, QColor, QImage, QPalette, QPen, QCursor
+
 
 class GradientBorderFrame(QFrame):
     def __init__(self, gradient_color1, gradient_color2, parent=None):
@@ -22,7 +24,6 @@ class GradientBorderFrame(QFrame):
         pen = QPen(QBrush(gradient), 4)  # Ширина обводки здесь 4 пикселя, можно изменить
         painter.setPen(pen)
         painter.drawRoundedRect(self.rect(), 20, 20)  # 20 - радиус скругления углов, можно изменить
-
 
         # Остальной код без изменений
 
@@ -58,6 +59,7 @@ class GradientBorderFrame(QFrame):
         self.anim.setEasingCurve(QEasingCurve.OutCubic)
         self.anim.start()
 
+
 class GroupWidget(QWidget):
     def __init__(self, icon_path, gradient_color1, gradient_color2, label_text, description_texts, parent=None):
         super().__init__(parent)
@@ -69,18 +71,18 @@ class GroupWidget(QWidget):
         width_widget.setFixedSize(250, 1)
         layout.addWidget(width_widget)
 
-        #hex_color = gradient_color1
+        # hex_color = gradient_color1
 
         # Преобразование HEX в RGB
-        #rgb_color = tuple(int(hex_color[i:i + 2], 16) for i in (1, 3, 5))
+        # rgb_color = tuple(int(hex_color[i:i + 2], 16) for i in (1, 3, 5))
 
         outer_frame = GradientBorderFrame(gradient_color1, gradient_color2, self)
         outer_frame.setCursor(QCursor(Qt.PointingHandCursor))
         outer_frame.setStyleSheet(
             "QWidget:hover {"
             f"   background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 rgba({gradient_color2.red()}, {gradient_color2.green()}, {gradient_color2.blue()}, 0.4), stop:1 rgba({gradient_color1.red()}, {gradient_color1.green()}, {gradient_color1.blue()}, 0.4));"  # Градиент с прозрачным начальным цветом и прозрачностью в конечном цвете
-                                                                                                                                               "   border-radius: 17px;"
-                                                                                                                                               "}"
+            "   border-radius: 17px;"
+            "}"
 
         )
         outer_frame.setWindowOpacity(0.1)
@@ -107,7 +109,7 @@ class GroupWidget(QWidget):
         line_layout.addSpacerItem(QSpacerItem(30, 1, QSizePolicy.Fixed, QSizePolicy.Fixed))  # Add left margin
         gradient_line = QFrame()
         gradient_line.setFixedWidth(2)
-        gradient_line.setFixedHeight(50)# Set fixed width to 250 pixels
+        gradient_line.setFixedHeight(50)  # Set fixed width to 250 pixels
         gradient_line.setFrameShape(QFrame.HLine)
         gradient_line.setStyleSheet(
             "border: 0px; background: qlineargradient(x1:0, y1:0, x2:1, y2:0, stop:0 {}, stop:1 {}); border-radius: 1px;".format(
@@ -119,9 +121,10 @@ class GroupWidget(QWidget):
         spacer_item = QSpacerItem(20, 30, QSizePolicy.Minimum, QSizePolicy.Expanding)
         layout_outer_frame.addItem(spacer_item)
 
-        new_label = QLabel(label_text)
-        new_label.setAlignment(Qt.AlignCenter)
-        layout_outer_frame.addWidget(new_label, alignment=Qt.AlignCenter)
+        name = QLabel(label_text)
+        name.setAlignment(Qt.AlignCenter)
+        name.setStyleSheet("font-size: 16pt;")
+        layout_outer_frame.addWidget(name, alignment=Qt.AlignCenter)
 
         spacer_item = QSpacerItem(20, 30, QSizePolicy.Minimum, QSizePolicy.Expanding)
         layout_outer_frame.addItem(spacer_item)
@@ -129,7 +132,7 @@ class GroupWidget(QWidget):
         for text, color in description_texts:
             label = QLabel(text)
             label.setAlignment(Qt.AlignCenter)
-            label.setStyleSheet("color: {}; border: 0px; background: transparent;".format(color))
+            label.setStyleSheet("color: {}; border: 0px; background: transparent; font-size: 10pt;".format(color))
             layout_outer_frame.addWidget(label, alignment=Qt.AlignCenter)
 
         icon_widget.setStyleSheet("""
@@ -138,8 +141,6 @@ class GroupWidget(QWidget):
                         background: transparent;
                     }
                 """)
-
-
 
     def load_and_render_svg(self, filename, gradient_color1, gradient_color2):
         image = QImage(filename)
@@ -164,6 +165,7 @@ class GroupWidget(QWidget):
 
         return pixmap
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -173,26 +175,31 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
+        # Устанавливаем стили
         self.setStyleSheet("""
-                    QMainWindow {
-                        background-color: #1C1B1B; /* черно-серый цвет фона */
-                    }
-                    QPushButton {
-                        background-color: #191919; /* серый цвет кнопок */
-                        color: white; /* белый цвет текста на кнопках */
-                        border-radius: 5px; /* скругление углов кнопок */
-                        padding: 10px; /* отступ вокруг текста кнопок */
-                        border: 1px solid #7E7E7E;
-                    }
-                    QPushButton:hover {
-                        background-color: #111111; /* изменение цвета при наведении на кнопку */
-                    }
-                    QLabel {
-                        color: white; /* устанавливаем белый цвет текста на иконках */
-                    }
-                """)
+            QMainWindow {
+                background-color: #1C1B1B; /* черно-серый цвет фона */
+            }
+            QPushButton {
+                background-color: #191919; /* серый цвет кнопок */
+                color: white; /* белый цвет текста на кнопках */
+                border-radius: 5px; /* скругление углов кнопок */
+                padding: 10px; /* отступ вокруг текста кнопок */
+                border: 1px solid #7E7E7E;
+            }
+            QPushButton:hover {
+                background-color: #111111; /* изменение цвета при наведении на кнопку */
+            }
+            QLabel {
+                color: white; /* устанавливаем белый цвет текста на иконках */
+            }
+        """)
 
+        # Изменяем на горизонтальный компоновщик
         main_layout = QHBoxLayout(central_widget)
+
+        # Создаем горизонтальный компоновщик для групп кнопок
+        horizontal_layout = QHBoxLayout()
 
         # Группа 1
         group1_widget = GroupWidget("icons/pc.svg", QColor("#D660F2"), QColor("#5A42D6"), "Оффлайн",
@@ -201,9 +208,8 @@ class MainWindow(QMainWindow):
                                         ("✅ Простая настройка", "#D3D3D3"),
                                         ("⚠️ Локальный доступ", "#D3D3D3"),
                                         ("❌ Низкая безопасность БД", "#D3D3D3"),
-                                        
                                     ])
-        main_layout.addWidget(group1_widget)
+        horizontal_layout.addWidget(group1_widget)
 
         # Группа 2
         group2_widget = GroupWidget("icons/network.svg", QColor("#6942D6"), QColor("#29B2D5"), "Децентрализованный",
@@ -213,7 +219,7 @@ class MainWindow(QMainWindow):
                                         ("⚠️ Доступ к сети", "#D3D3D3"),
                                         ("❌ Требуется открытая MySQL БД", "#D3D3D3")
                                     ])
-        main_layout.addWidget(group2_widget)
+        horizontal_layout.addWidget(group2_widget)
 
         # Группа 3
         group3_widget = GroupWidget("icons/internet.svg", QColor("#2667C9"), QColor("#46C1F8"), "Централизованный",
@@ -223,7 +229,28 @@ class MainWindow(QMainWindow):
                                         ("✅ Крайне высокая безопасность БД", "#D3D3D3"),
                                         ("⚠️ Доступ к сети", "#D3D3D3")
                                     ])
-        main_layout.addWidget(group3_widget)
+        horizontal_layout.addWidget(group3_widget)
+
+        main_layout.addLayout(horizontal_layout)
+
+        # Создаем лэйбл
+        self.common_label = QLabel("Выберите режим", self)
+        self.common_label.setStyleSheet("font-size: 24px; color: white;")
+
+        # Перемещаем лэйбл по центру над группами кнопок
+        self.adjust_label_position()
+
+    def resizeEvent(self, event):
+        # Пересчитываем положение метки при изменении размера окна
+        self.adjust_label_position()
+
+    def adjust_label_position(self):
+        # Перемещаем метку по центру по вертикали
+        self.common_label.adjustSize()
+        x = (self.width() - self.common_label.width()) // 2
+        y = (self.height() - self.common_label.height()) // 20
+        self.common_label.move(x, y)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
