22fcfcaa848a059a8ea1ccfbab05e24f5ca5d68e
ArmedWings <armedwings@yandex.ru>
2024-05-05 10:45:09 +0300

    Changed UI for TestEditor page

--- TestEditor.py
+++ TestEditor.py
@@ -1,6 +1,7 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QDateEdit
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTimeEdit
+from PySide6.QtCore import Qt, QTime
+from PySide6 import QtGui
 
 
 class TestEditor(QWidget):
@@ -19,6 +20,7 @@ class TestEditor(QWidget):
         # Лэйбл "Редактирование теста"
         label_editing = QLabel("Редактирование теста", self)
         label_editing.setAlignment(Qt.Alignment.AlignCenter)  # Выравнивание по центру
+        label_editing.setFont(QtGui.QFont("Arial", 16))  # Установка крупного шрифта
         vertical_layout.addWidget(label_editing)
 
         vertical_layout.addSpacing(50)
@@ -26,37 +28,43 @@ class TestEditor(QWidget):
         # Горизонтальные компоновщики для каждой пары лейбл-поле ввода
 
         # Название теста
-        test_name_layout = QHBoxLayout()
         label_test_name = QLabel("Название теста:", self)
+        label_test_name.setFont(QtGui.QFont("Arial", 12))  # Установка крупного шрифта
         self.test_name_edit = QLineEdit(self)
-        test_name_layout.addWidget(label_test_name)
-        test_name_layout.addWidget(self.test_name_edit)
-        vertical_layout.addLayout(test_name_layout)
+        vertical_layout.addWidget(label_test_name)
+        vertical_layout.addWidget(self.test_name_edit)
 
         # Горизонтальный компоновщик для "Количество попыток" и "Время на прохождение теста"
-        input_layout = QHBoxLayout()
 
         # Вертикальный компоновщик для "Количество попыток"
-        attempts_layout = QVBoxLayout()
+        labels_layout = QHBoxLayout()
         label_attempts = QLabel("Количество попыток:", self)
-        self.attempts_edit = QLineEdit(self)
-        attempts_layout.addWidget(label_attempts)
-        attempts_layout.addWidget(self.attempts_edit)
-        self.attempts_edit.setFixedWidth(100)  # Фиксированная ширина для поля ввода
-        attempts_layout.setAlignment(Qt.Alignment.AlignLeft)
-        input_layout.addLayout(attempts_layout)
+        label_attempts.setFont(QtGui.QFont("Arial", 12))  # Установка крупного шрифта
+        label_time = QLabel("Время на прохождение теста (чч:мм:сс):", self)
+        label_time.setFont(QtGui.QFont("Arial", 12))  # Установка крупного шрифта
+        labels_layout.addWidget(label_attempts)
+        labels_layout.addStretch(1)
+        labels_layout.addWidget(label_time)
+        label_attempts.setAlignment(Qt.Alignment.AlignLeft)
+        label_time.setAlignment(Qt.Alignment.AlignRight)
 
         # Вертикальный компоновщик для "Время на прохождение теста"
-        time_layout = QVBoxLayout()
-        label_time = QLabel("Время на прохождение теста:", self)
-        self.time_edit = QDateEdit(self)
-        time_layout.addWidget(label_time)
-        time_layout.addWidget(self.time_edit)
+        pickers_layout = QHBoxLayout()
+        self.time_edit = QTimeEdit(self)
+        self.time_edit.setDisplayFormat("HH:mm:ss")  # Устанавливаем формат отображения времени
+        self.time_edit.setTime(QTime(0, 0, 0))  # Устанавливаем начальное время 00:00:00
+        self.attempts_edit = QLineEdit(self)
+        self.attempts_edit.setInputMask("99")
+        self.attempts_edit.setFixedWidth(30)  # Фиксированная ширина для поля ввода
         self.time_edit.setFixedWidth(100)  # Фиксированная ширина для поля ввода
-        time_layout.setAlignment(Qt.Alignment.AlignRight)
-        input_layout.addLayout(time_layout)
+        pickers_layout.addWidget(self.attempts_edit)
+        pickers_layout.addStretch(1)
+        pickers_layout.addWidget(self.time_edit)
+        self.time_edit.setAlignment(Qt.Alignment.AlignRight)
+        self.attempts_edit.setAlignment(Qt.Alignment.AlignLeft)
 
-        vertical_layout.addLayout(input_layout)
+        vertical_layout.addLayout(labels_layout)
+        vertical_layout.addLayout(pickers_layout)
 
         # Кнопки
         buttons_layout = QHBoxLayout()
